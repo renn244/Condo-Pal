13d9fc6cee053ede175bf6135b790c8e22e0c498
Renato D. Santos Jr <renatodsantosjr9@gmail.com>
2025-04-15 21:39:45 +0800

    feature: added modern sidebar in the landlord dashboard

--- frontend/src/components/pageComponents/dashboard/Sidebar.tsx
+++ frontend/src/components/pageComponents/dashboard/Sidebar.tsx
@@ -1,39 +1,141 @@
-import { CreditCard, Hotel, LayoutDashboard, MessageSquare, Wrench } from "lucide-react"
-import { NavLink } from "react-router-dom"
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import { DropdownMenu, DropdownMenuContent, DropdownMenuGroup, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "@/components/ui/dropdown-menu"
+import { Sidebar, SidebarContent, SidebarFooter, SidebarGroup, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem, useSidebar } from "@/components/ui/sidebar"
+import { useAuthContext } from "@/context/AuthContext"
+import { BadgeCheck, Bell, Building2, CreditCard, Hotel, LayoutDashboard, LogOut, MessageSquare, Sparkles, Wrench } from "lucide-react"
+import { Link } from "react-router-dom"
 
-const DashboardSidebar = () => {
+const DashboardSidebar = ({
+    ...props
+}: React.ComponentProps<typeof Sidebar>) => {
+    const { user } = useAuthContext();
+    const { isMobile } = useSidebar();
+    const location = window.location.pathname;
+    const path = "/" + location.split("/").slice(1).join("/");
 
+    const logOut = () => {
+        // delete all the tokens
+        localStorage.removeItem('access_token');
+        localStorage.removeItem('refresh_token');
+    
+        window.location.reload();
+    }
+    
     const sidebarLinks = [
-        { icon: LayoutDashboard, label: 'Dashboard', href: '/dashboard/dashboard' },
-        { icon: Hotel, label: 'Condo', href: '/dashboard/condo' },
-        { icon: Wrench , label: 'Maintenance', href: '/dashboard/maintenance' },
-        { icon: CreditCard, label: 'Payments', href: '/dashboard/payments'  },
-        { icon: MessageSquare, label: 'Chats', href: '/dashboard/chats' }
+        { icon: LayoutDashboard, title: 'Dashboard', url: '/dashboard/dashboard' },
+        { icon: Hotel, title: 'Condo', url: '/dashboard/condo' },
+        { icon: Wrench , title: 'Maintenance', url: '/dashboard/maintenance' },
+        { icon: CreditCard, title: 'Payments', url: '/dashboard/payments'  },
+        { icon: MessageSquare, title: 'Chats', url: '/dashboard/chats' }
     ]
 
     return (
-        <div className="min-h-[851px] flex flex-col h-auto bg-primary text-primary-foreground w-64 py-8 px-4">
-            <nav className="flex-1">
-                <div className="mb-8">
-                    <h1 className="text-2xl font-bold text-center">Dashboard</h1>
-                </div>
-                <ul className="space-y-1">
-                    {sidebarLinks.map((item) => (
-                        <li key={item.label}>
-                            <NavLink
-                            to={item.href}
-                            className={({ isActive }) => `
-                                flex items-center space-x-3 p-3 rounded-lg hover:bg-blue-700 transition-colors ${isActive ? "bg-blue-700" : ""}
-                            `}
+        <Sidebar collapsible="icon" {...props}>
+            <SidebarHeader>
+                <SidebarMenu>
+                    <SidebarMenuItem>
+                        <SidebarMenuButton size="lg" asChild>
+                            <Link to="/">
+                                <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
+                                    <Building2 className="size-4 font-thin" />
+                                </div>
+                                <div className="flex flex-col gap-0.5 leading-none">
+                                    <span className="font-semibold">CondoPal</span>
+                                </div>
+                            </Link>
+                        </SidebarMenuButton>
+                    </SidebarMenuItem>
+                </SidebarMenu>
+            </SidebarHeader>
+            <SidebarContent>
+                <SidebarGroup>
+                    <SidebarMenu>
+                        {sidebarLinks.map((item) => (
+                            <SidebarMenuItem key={item.title}>
+                                <SidebarMenuButton isActive={path === item.url} asChild>
+                                    <Link to={item.url}>
+                                        {item.icon && <item.icon />}
+                                        <span>{item.title}</span>
+                                    </Link>
+                                </SidebarMenuButton>
+                            </SidebarMenuItem>
+                        ))}
+                    </SidebarMenu>
+                </SidebarGroup>
+            </SidebarContent>
+            <SidebarFooter>
+                <SidebarMenu>
+                    <SidebarMenuItem>
+
+                    <DropdownMenu>
+                        <DropdownMenuTrigger asChild>
+                            <SidebarMenuButton 
+                            size="lg"
+                            className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
                             >
-                                <item.icon className="h-5 w-5" />
-                                <span>{item.label}</span>
-                            </NavLink>
-                        </li>
-                    ))}
-                </ul>
-            </nav>
-        </div>
+                            <Avatar className="h-8 w-8 rounded-lg">
+                                <AvatarImage src={user?.profile} alt={user?.name} />
+                                <AvatarFallback className="rounded-lg">{user?.name?.charAt(0)}</AvatarFallback>
+                            </Avatar>
+                            <div className="grid flex-1 text-left text-sm leading-tight">
+                                <span className="truncate font-semibold">{user?.name}</span>
+                                <span className="truncate text-xs">{user?.email}</span>
+                            </div>
+                            </SidebarMenuButton>
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent
+                        className="w-[--radix-dropdown-menu-trigger-width] min-w-56 rounded-lg"
+                        side={isMobile ? "bottom" : "right"}
+                        align="end"
+                        sideOffset={4}
+                        >
+                            <DropdownMenuLabel className="p-0 font-normal">
+                                <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
+                                    <Avatar className="h-8 w-8 rounded-lg">
+                                        <AvatarImage src={user?.profile} alt={user?.name} />
+                                        <AvatarFallback className="rounded-lg">{user?.name?.charAt(0)}</AvatarFallback>
+                                    </Avatar>
+                                    <div className="grid flex-1 text-left text-sm leading-tight">
+                                        <span className="truncate font-semibold">{user?.name}</span>
+                                        <span className="truncate text-xs">{user?.email}</span>
+                                    </div>
+                                </div> 
+                                </DropdownMenuLabel>
+                            <DropdownMenuSeparator />
+
+                            <DropdownMenuGroup>
+                                <DropdownMenuItem>
+                                    <Sparkles />
+                                    Upgrade to Pro
+                                </DropdownMenuItem>
+                            </DropdownMenuGroup>
+                            <DropdownMenuSeparator />
+                            <DropdownMenuGroup>
+                                <DropdownMenuItem>
+                                    <BadgeCheck />
+                                    Account
+                                </DropdownMenuItem>
+                                <DropdownMenuItem>
+                                    <CreditCard />
+                                    Billing
+                                </DropdownMenuItem>
+                                <DropdownMenuItem>
+                                    <Bell />
+                                    Notifications
+                                </DropdownMenuItem>
+                            </DropdownMenuGroup>
+                            <DropdownMenuSeparator />
+                            <DropdownMenuItem onClick={() => logOut()}>
+                                <LogOut />
+                                Log out
+                            </DropdownMenuItem>
+                        </DropdownMenuContent>
+                    </DropdownMenu>
+
+                    </SidebarMenuItem>
+                </SidebarMenu>
+            </SidebarFooter>
+        </Sidebar>
     )
 }
 
--- frontend/src/page/Dashboard/Dashboard.tsx
+++ frontend/src/page/Dashboard/Dashboard.tsx
@@ -1,25 +1,58 @@
 import DashboardSidebar from "@/components/pageComponents/dashboard/Sidebar"
-import { Route, Routes } from "react-router-dom"
+import { Route, Routes, useLocation } from "react-router-dom"
 import Condo from "./Condo"
 import Maintenance from "./Maintenance"
 import Payments from "./Payments"
 import ViewCondo from "./ViewCondo"
 import Chat from "./Chat"
+import { SidebarInset, SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
+import { Separator } from "@/components/ui/separator"
+import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbPage, BreadcrumbSeparator } from "@/components/ui/breadcrumb"
 
 const Dashboard = () => {
+    const location = useLocation();
+    const path = location.pathname.split("/").slice(2).join("/");
+
     return (
         <div className="min-h-[853px] flex">
-            <DashboardSidebar />
-            <main className="w-full min-h-[853px] p-4">
-                <Routes>
-                    <Route path="/dashboard" element={undefined} />
-                    <Route path="/condo" element={<Condo />} />
-                    <Route path="/condo/:condoId" element={<ViewCondo />} />
-                    <Route path="/maintenance" element={<Maintenance />} />
-                    <Route path="/payments" element={<Payments />} />
-                    <Route path="/chats" element={<Chat />} />
-                </Routes>
-            </main>
+            <SidebarProvider>
+                <DashboardSidebar />
+                <SidebarInset>
+                    <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12 border-b">
+                        <div className="flex items-center justify-between px-4 w-full">
+                            <div className="flex items-center gap-2">
+                                <SidebarTrigger className="-ml-1" />
+                                <Separator orientation="vertical" className="mr-2 h-4" />
+                                <Breadcrumb>
+                                    <BreadcrumbList>
+                                        <BreadcrumbItem className="hidden md:block">
+                                            <BreadcrumbLink href="#">
+                                                Landlord
+                                            </BreadcrumbLink>
+                                        </BreadcrumbItem>
+                                        <BreadcrumbSeparator className="hidden md:block" />
+                                        <BreadcrumbItem>
+                                            <BreadcrumbPage>
+                                                {path}
+                                            </BreadcrumbPage>
+                                        </BreadcrumbItem>
+                                    </BreadcrumbList>
+                                </Breadcrumb>
+                            </div>
+                        </div>
+                    </header>
+                    <main className="w-full min-h-[853px] p-4">
+                        <Routes>
+                            <Route path="/dashboard" element={undefined} />
+                            <Route path="/condo" element={<Condo />} />
+                            <Route path="/condo/:condoId" element={<ViewCondo />} />
+                            <Route path="/maintenance" element={<Maintenance />} />
+                            <Route path="/payments" element={<Payments />} />
+                            <Route path="/chats" element={<Chat />} />
+                        </Routes>
+                    </main>
+                </SidebarInset>
+            </SidebarProvider>
         </div>
     )
 }
